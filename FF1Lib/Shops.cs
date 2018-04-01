1bc1ed1cac10a2a32cc107df836baf65ea9e1ecc
David Moeller <dhmoeller@gmail.com>
2018-04-01 18:42:57 -0500

    Randomize instead of shuffle for weapon and armor shops

--- FF1Lib/Shops.cs
+++ FF1Lib/Shops.cs
@@ -32,14 +32,14 @@ namespace FF1Lib
 			Item = 60
 		}
 
-		public ItemShopSlot ShuffleShops(MT19337 rng, bool earlyAilments)
+		public ItemShopSlot ShuffleShops(MT19337 rng, bool earlyAilments, bool randomizeWeaponsAndArmor)
 		{
 			var pointers = Get(ShopPointerOffset, ShopPointerCount * ShopPointerSize).ToUShorts();
 
 			RepackShops(pointers);
 
-			ShuffleShopType(ShopType.Weapon, pointers, rng);
-			ShuffleShopType(ShopType.Armor, pointers, rng);
+			ShuffleShopType(ShopType.Weapon, pointers, rng, randomizeWeaponsAndArmor);
+			ShuffleShopType(ShopType.Armor, pointers, rng, randomizeWeaponsAndArmor);
             ItemShopSlot result = null;
 			do
 			{
@@ -104,7 +104,7 @@ namespace FF1Lib
 			Put(ShopPointerBase + pointers[0], allEntries.ToArray());
 		}
 
-		private ItemShopSlot ShuffleShopType(ShopType shopType, ushort[] pointers, MT19337 rng)
+		private ItemShopSlot ShuffleShopType(ShopType shopType, ushort[] pointers, MT19337 rng, bool randomize = false)
 		{
 			var shops = GetShops(shopType, pointers);
 
@@ -151,6 +151,16 @@ namespace FF1Lib
 				}
 			} while (shopsBlocked);
 
+			if (randomize)
+			{
+				if (shopType == ShopType.Weapon || shopType == ShopType.Armor) {
+					var baseIndex = shopType == ShopType.Armor ? 67 : 27;
+					for (int i = 0; i < newShops.Length; i++)
+					{
+						newShops[i] = newShops[i].Select(x => (byte) (baseIndex + rng.Between(1, 40))).Distinct().ToList();
+					}
+				}
+			}
 			// Zero-terminate the new shops.
 			foreach (var newShop in newShops)
 			{
