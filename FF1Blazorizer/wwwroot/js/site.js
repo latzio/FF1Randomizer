c721ad2b6954e23833e02771690009b49cbc93c7
Mike Lattanzio <latzio@gmail.com>
2018-11-10 00:58:41 -0500

    First crack at fun percent backup.

--- FF1Blazorizer/wwwroot/js/site.js
+++ FF1Blazorizer/wwwroot/js/site.js
@@ -1,11 +1,9 @@
-﻿function handleFileSelect(inputId) {
-
-	var input = document.getElementById(inputId);
-	var file = input.files[0];
-	var reader = new FileReader();
+﻿async function handleFileSelect(inputId) {
+	const input = document.getElementById(inputId);
+	const file = input.files[0];
+	const reader = new FileReader();
 
 	return new Promise((resolve, reject) => {
-
 		reader.onload = e => {
 			let encoded = e.target.result.split(',')[1];
 			resolve(encoded);
@@ -13,25 +11,39 @@
 		reader.onerror = () => {
 			reader.abort();
 			reject(new DOMException("Error reading file"));
-		}
+		};
 
 		reader.readAsDataURL(file);
-
 	});
-
 }
 
-function downloadROM(filename, encoded) {
-	var url = "data:application/octet-stream;base64," + encoded;
-	fetch(url)
-		.then(result => result.blob())
-		.then(blob => {
+async function downloadROM(filename, encoded) {
+	const url = "data:application/octet-stream;base64," + encoded;
+	const result = await fetch(url);
+	const blob = await result.blob();
 
-			var anchor = document.createElement('a');
+	var anchor = document.createElement('a');
+	anchor.download = filename;
+	anchor.href = window.URL.createObjectURL(blob);
+	anchor.dispatchEvent(new MouseEvent('click'));
+}
 
-			anchor.download = filename;
-			anchor.href = window.URL.createObjectURL(blob);
-			anchor.dispatchEvent(new MouseEvent('click'));
+const funKey = 'fun';
 
-		});
+function savePreferences(fun) {
+	localStorage.setItem(funKey, JSON.stringify(fun));
+	document.getElementById('funKeyMessage').innerText = 'Preferences Saved.';
+}
+
+function clearPreferences() {
+	localStorage.clear(funKey);
+	document.getElementById('funKeyMessage').innerText = 'Preferences Cleared.';
+}
+
+function loadPreferences() {
+	let fun = localStorage.getItem(funKey);
+	fun = fun ? JSON.parse(fun) : {}
+
+	document.getElementById('funKeyMessage').innerText = 'Your preferred Fun % flags have been automatically restored.';
+	return fun;
 }
